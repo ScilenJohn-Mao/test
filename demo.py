f038c86b105f4b01d168525e76301e324c8ca755
ScilenJohn-Mao <spritM@outlook,com>
2023-03-08 19:17:37 +0800

    测试

---
+++ demo.py
@@ -0,0 +1,14 @@
+def test1(a,b):
+    c = a / b
+    return c
+def test2(a,b):
+    c = a / b
+    return c
+
+if __name__ == '__main__':
+    a = 1
+    b = 1
+    test1(a,b)
+    b = 0
+    test2(a,b)
+    
